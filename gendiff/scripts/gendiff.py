044562623e986b05ce440c8ce8211bd284dce4f8
NoFate35 <vsokol35@mail.ru>
2024-03-16 18:15:48 +0300

    Changes to be committed: 	new file:   .gitignore 	new file:   Makefile 	new file:   gendiff/scripts/gendiff.py 	new file:   poetry.lock 	new file:   pyproject.toml 	Add --help and positional args

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+
+import argparse
+
+def main():
+        parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+        parser.add_argument('first_file', type = str, help="in file")
+        parser.add_argument('second_file', type = str, help="out file")
+        args = parser.parse_args()
+        print(args. first_file, args.second_file)
+        print("Hello!")
+
+if __name__ == "main":
+    main()
