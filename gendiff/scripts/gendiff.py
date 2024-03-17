955724134d31659bcbe2435511b2f1d1121405f7
NoFate35 <vsokol35@mail.ru>
2024-03-17 09:34:13 +0300

    Changes to be committed: 	modified:   gendiff/scripts/gendiff.py 	modified:   poetry.lock 	modified:   pyproject.toml 	new file:   setup.cfg
    	Add flake8 functionality, add option arguments

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import argparse
 
+
 def main():
-        parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-        parser.add_argument('first_file', type = str, help="in file")
-        parser.add_argument('second_file', type = str, help="out file")
-        args = parser.parse_args()
-        print(args. first_file, args.second_file)
-        print("Hello!")
+    parser = argparse.ArgumentParser(
+                                     description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', type=str, help="in file")
+    parser.add_argument('second_file', type=str, help="out file")
+    parser.add_argument("-f", "--format", type=str, help="set format of output")
+    parser.parse_args()
+
 
 if __name__ == "main":
     main()
