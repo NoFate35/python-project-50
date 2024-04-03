acfd19c0fea31340e214e416efdf3b1bd71bf3a5
NoFate35 <vsokol35@mail.ru>
2024-04-03 14:27:29 +0300

    Changes to be committed: 	modified:   gendiff/diff/get_diff.py 	new file:   tests/fixtures/result.txt 	modified:   tests/test_get_diff.py
    	Add tests

--- gendiff/diff/get_diff.py
+++ gendiff/diff/get_diff.py
@@ -11,16 +11,17 @@ def generate_diff(file_path1, file_path2):
         visited.add(key1)
         if key1 in file_object2.keys():
             if value1 == file_object2[key1]:
-                exit.append(f"   {key1}: {value1}\n")
+                exit.append(f"    {key1}: {value1}\n")
             else:
-                exit.append(f" - {key1}: {value1}\n")
-                exit.append(f" + {key1}: {file_object2[key1]}\n")
+                exit.append(f"  - {key1}: {value1}\n")
+                exit.append(f"  + {key1}: {file_object2[key1]}\n")
         else:
-            exit.append(f" - {key1}: {value1}\n")
+            exit.append(f"  - {key1}: {value1}\n")
+            #print(f"- {key1}: {value1}\n ")
     for key2, value2 in file_object2.items():
         if key2 in visited:
             continue
         else:
-            exit.append(f" + {key2}: {value2}\n")
-    exit.sort(key=lambda x: x[3])
+            exit.append(f"  + {key2}: {value2}\n")
+    exit.sort(key=lambda x: x[4])
     return ''.join(['{\n'] + exit + ['}'])
--- tests/test_get_diff.py
+++ tests/test_get_diff.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python
 import pytest
 from gendiff.diff.get_diff import generate_diff
 
     
 @pytest.fixture
 def files():
-    return ("tests/fixtures/file1.json", "tests/fixtures/file2.json")
+    return ("tests/fixtures/file1.json", "tests/fixtures/file2.json", "tests/fixtures/result.txt")
         
         
 def test_generate_diff(files):
-    first_file, second_file = files
-    with open(first_file, "r") as file1:
-        print(file1.read())
-    with open(second_file, "r") as file2:
-        print(file2.read())
+    first_file, second_file, result = files
+    with (
+               open(first_file, "r" ) as file1,
+               open(second_file, "r") as file2,
+               open(result, "r") as result,
+    ):
+        assert generate_diff(first_file, second_file) == result.read()
