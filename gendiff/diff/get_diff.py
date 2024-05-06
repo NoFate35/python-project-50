3338f1ba36cd9c96166b6af66a24f86d24d18c43
NoFate35 <vsokol35@mail.ru>
2024-05-06 06:00:25 +0300

    Changes to be committed: 	modified:   gendiff/diff/get_diff.py 	modified:   gendiff/diff/stylish.py
    	work for formatter

--- gendiff/diff/get_diff.py
+++ gendiff/diff/get_diff.py
@@ -2,46 +2,43 @@ from gendiff.diff.parse import get_data
 from gendiff.diff.stylish import formatter1
 
 
-def make_value(value, file_extention):
-    if value is False:
-        value = 'false'
-    elif (value is True) and (file_extention == ".yml"):
-        value = 'true'
-    return value
-
-
 def generate_diff(file_path1, file_path2):
     data1, data2, file_extention = get_data(file_path1, file_path2)
     """get paths to files and return difference of them fot .txt string"""
     return get_diff_dict(data1, data2, file_extention)
     
 def get_diff_dict(data1, data2, file_extention):
-    def walk(tree1, tree2, file_extention):
+    def walk(tree1, tree2, file_extention, flag):
         #print("tree1", tree1, "tree2", tree2, sep='\n')
+        if tree2 == {}:
+        	flag = "m"
+        #print("flag", flag)
         string = []
         visited = set()
         for key1 in  tree1.keys():
+                #print("for flag", flag)
                 #print("for key1 in tree1.keys(): key1")
                 visited.add(key1)
                 if key1 in tree2.keys():
                     #print("if key1 in tree2.keys() key1", key1)
                     if isinstance(tree1[key1], dict) == True:
                         if isinstance(tree2[key1], dict) == True:
+                            
                             string.append({
                             "m": {
                            "first": {"parent": key1,
-                            "children": [walk(tree1[key1], tree2[key1], file_extention)]}}})
+                            "children": walk(tree1[key1], tree2[key1], file_extention, "u")}}})
                         else:
                         	string.append({
-                            "u": {
-                            "first": {"parent": key1, "children": [walk(tree1[key1], {}, file_extention)]}, 
+                            flag: {
+                            "first": {"parent": key1, "children": walk(tree1[key1], {}, file_extention, flag)}, 
                             "second": {"key": key1, "value": tree2[key1] }}})
                     else:
                         if isinstance(tree2[key1], dict) == True:
                         	string.append({
-                            "u": {
+                            flag: {
                             "first": {"key": key1, "value": tree1[key1]}, 
-                            "second": {"parent": key1, "children": [walk({}, tree2[key1], file_extention)] }}})
+                            "second": {"parent": key1, "children": walk(tree2[key1], {}, file_extention, flag) }}})
                         else:
                             if tree1[key1] == tree2[key1]:
                             	string.append({
@@ -49,7 +46,7 @@ def get_diff_dict(data1, data2, file_extention):
                             "first": {"key": key1, "value": tree1[key1]}}})
                             else:
                             	string.append({
-                            "u": {
+                            flag: {
                             "first": {"key": key1, "value": tree1[key1]}, 
                             "second": {"key": key1, "value": tree2[key1]}}})
         
@@ -57,25 +54,27 @@ def get_diff_dict(data1, data2, file_extention):
                 else:
                     if isinstance(tree1[key1], dict) == True:
                             string.append({
-                            "u": {
-                            "first": {"parent": key1, "children": [walk(tree1[key1], {}, file_extention)]}}})
+                            flag: {
+                            "first": {"parent": key1, "children": walk(tree1[key1], {}, file_extention, flag)}}})
                     else:
+                            #print("ffflllaaggg", flag, "value", tree1[key1])
                             string.append({
-                            "u": {
+                            flag: {
                             "first": {"key": key1, "value": tree1[key1]}}})
+        flag = "u"
         for key2 in tree2.keys():
             if key2 in visited:
             				pass
             elif isinstance(tree2[key2], dict) == True:
                             string.append({
-                            "u": {
-                            "second": {"parent": key2, "children": [walk({}, tree2[key2], file_extention)] }}})
+                            flag: {
+                            "second": {"parent": key2, "children": walk(tree2[key2], {}, file_extention, flag) }}})
             else:
                             string.append({
-                            "u": {
+                            flag: {
                             "second": {"key": key2, "value": tree2[key2]}}})
         #print("string", string["parent1"], "dept", dept)
         return string
-    exit = walk(data1, data2, file_extention)
+    exit = walk(data1, data2, file_extention, "u")
     #print("get_diff", exit)
     return formatter1(exit, file_extention)
--- gendiff/diff/stylish.py
+++ gendiff/diff/stylish.py
@@ -1,23 +1,130 @@
-def formatter1(exit, file_extention):
-    dept = 1
-    def walk(exit, file_extention, dept):
-    	for comparsion in exit:
+def make_value(value, file_extention):
+    if value is False:
+        value = 'false'
+    elif value is True:
+        value = 'true'
+    elif value is None:
+    	value = "null"
+    return value
+
+def get_match(comparsion):
+	char = comparsion.get("m")
+	if char is None:
+		return "u"
+	return "m"
+	
+	
+def get_key(string):
+	#print("string", string)
+	key = string.get("key")
+	if key is None:
+		return "parent"
+	return "key"
+	
+	
+def sort_tree(ls):
+	#print("la", ls)
+	comparsion = ls[get_match(ls)]
+	first = comparsion.get("first")
+	second = comparsion.get("second")
+	if first:
+		parent = first.get("parent")
+		if parent:
+			#print("parent[0]", parent[0])
+			return parent
+		else:
+			#print("first['key''][0]", first["key"][0])
+			return first["key"]
+	else:
+		parent = second.get("parent")
+		if parent:
+			#print("parent[0]", parent[0])
+			return parent
+		else:
+			#print("second['key''][0]", second["key"][0])
+			return second["key"]
+		
+
+def formatter1(tree, file_extention):
+    dept = 0
+    def walk(tree, file_extention, dept):
+    	#print("tree", tree)
+    	exit = []
+    	exit.append("".join(["{\n"]))
+    	sorted_tree = sorted(tree, key=sort_tree)
+    	#print("sorted_yree", sorted)
+    	for comparsion in sorted_tree:
+    		#print("compartion",  comparsion)
     		str = []
-    		str.append("...." * dept)
-    		match = comparsion.get("m")
-    		if match is None:
-    			match = comparsion["u"]
-    			str.append("-")
-    		else: str.append("+")
-    		#print("match", match)
-    		first = match.get("first")
-    		second = match.get("second")
-    		print("first", first, "second", second)
-    		"""
-    		parent = comparsion[match].get("parent")
-    		if parent is None:
-    			parent = "key"
-    		value =
-    		"""
-    	return
-    return walk(exit, file_extention, dept)
+    		match = get_match(comparsion)
+    		strings = comparsion[match]
+    		first = strings.get("first")
+    		second = strings.get("second")
+    		#print(" second", second)
+    		if (first is None) and (match == "u"):
+    			second_key = get_key(second)
+    			str.append("    " * dept )
+    			str.append("  ")
+    			str.append("+ ")
+    			if second_key == "parent":
+    				str.append(f"{second[second_key]}: ")
+    				str.append("".join(walk(second['children'], file_extention, dept + 1)))
+    				str.append("".join(["      " * dept, "  ", "}"]))
+    				str.append("\n")
+    			else:
+    				str.append(f"{second[second_key]}: ")
+    				str.append(f"{make_value(second['value'], file_extention)}")
+    				str.append("\n")
+    		elif (second is None) and (match == "u"):
+    			first_key = get_key(first)
+    			str.append("    " * dept )
+    			str.append("  ")
+    			str.append("- ")
+    			if first_key == "parent":
+    				str.append(f"{first[first_key]}: ")
+    				str.append("".join(walk(first['children'], file_extention, dept + 1)))
+    				#print("str", str)
+    			else:
+    				str.append(f"{first[first_key]}: ")
+    				str.append(f"{first['value']}")
+    				str.append("\n")
+    		elif (second is None) and (match == "m"):
+    			#print(" elif (second is None) and (match == 'm'):")
+    			first_key = get_key(first)
+    			str.append("".join(["    " * (dept + 1), f"{first[first_key]}: "]))
+    			if first_key == "parent":
+    				str.append("".join(walk(first['children'], file_extention, dept + 1)))
+    				str.append("".join(["    " * (dept + 1), "}"]))
+    				str.append("\n")
+    			else:
+    				#print("first_key not 'parent")
+    				str.append(f"{first['value']}")
+    				str.append("\n")
+    				#print("str", str)
+    		else:
+    				first_key = get_key(first)
+    				second_key = get_key(second)
+    				str.append("    " * dept )
+    				str.append("  ")
+    				str.append("- ")
+    				str.append(f"{first[first_key]}: ")
+    				if first_key == "parent":
+    					str.append("".join(walk(first['children'], file_extention, dept + 1)))
+    				else: 
+    				    str.append(f"{make_value(first['value'], file_extention)}")
+    				    str.append("\n")
+    				
+    				str.append("    " * dept )
+    				str.append("  ")
+    				str.append("+ ")
+    				str.append(f"{second[second_key]}: ")
+    				if second_key == "parent":
+    					str.append(f"{walk(second['children'], file_extention, dept + 1)}")
+    					str.append("".join(["\n", "    " * (dept - 1), "  ", "}"]))
+    				else: 
+    				    str.append(f"{make_value(second['value'], file_extention)}")
+    				    str.append("\n")
+    		exit.append("".join(str))
+    	return exit
+    print(print(walk(tree, file_extention, dept)))
+    return "".join(walk(tree, file_extention, dept))
