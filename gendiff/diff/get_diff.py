4baf53743f2c812e4feac45995502809cd7a53b3
NoFate35 <vsokol35@mail.ru>
2024-05-12 14:52:14 +0300

    Changes to be committed: 	modified:   gendiff/diff/get_diff.py
    	work for flake8

--- gendiff/diff/get_diff.py
+++ gendiff/diff/get_diff.py
@@ -4,76 +4,129 @@ from gendiff.diff.stylish import formatter1
 
 def generate_diff(file_path1, file_path2):
     data1, data2 = get_data(file_path1, file_path2)
-    """get paths to files and return difference of them fot .txt string"""
+    """get paths to files, transfer them to the
+    GET_DATA and receive files objects from
+    GET_DATA. give files objects to the
+    GET_DIFF_DICT and rerurn result string"""
     return get_diff_dict(data1, data2)
-    
-    
+
+
 def walk(tree1, tree2, flag):
-	if tree2 == {}:
-		flag = "m"
-	string = []
-	visited = set()
-	flag, string, visited = first_tree(tree1, tree2, flag, string, visited)
-	string = second_tree(tree2, visited, string)
-	return string
+    """generate the list of difference recursive and"""
+    """return his to GET DIFF DICT """
+    if tree2 == {}:
+        flag = "m"
+    string = []
+    visited = set()
+    flag, string, visited = first_tree(tree1, tree2, flag, string, visited)
+    string = second_tree(tree2, visited, string)
+    return string
+
+
+def if_key1_in_tree2(key1, tree1, tree2, flag, string):
+    if isinstance(tree1[key1], dict):
+        if isinstance(tree2[key1], dict):
+            string.append({
+                                         "m": {
+                                                   "first": {"parent": key1,
+                                                             "children":
+                                                             walk(tree1[key1],
+                                                                  tree2[key1],
+                                                                  "u")}}})
+        else:
+            string.append({
+                                        flag: {
+                                                   "first": {"parent": key1,
+                                                             "children":
+                                                             walk(tree1[key1],
+                                                                  {}, flag)},
+                                                   "second": {
+                                                                   "key": key1,
+                                                                   "value":
+                                                                   tree2[key1]
+                                                                   }}})
+    else:
+        if isinstance(tree2[key1], dict):
+            string.append({
+                flag: {
+                           "first": {
+                                     "key": key1,
+                                     "value": tree1[key1]},
+                           "second": {
+                                           "parent": key1,
+                                           "children":
+                                           walk(tree2[key1],
+                                                {}, flag)}}})
+        else:
+            if tree1[key1] == tree2[key1]:
+                string.append({
+                                             "m": {
+                                                   "first": {
+                                                            "key": key1,
+                                                            "value":
+                                                            tree1[key1]}}})
+            else:
+                string.append({
+                    flag: {
+                               "first": {
+                                         "key": key1,
+                                         "value": tree1[key1]},
+                               "second": {
+                                               "key": key1,
+                                               "value": tree2[key1]}}})
+    return string, flag
+
 
 def first_tree(tree1, tree2, flag, string, visited):
-		for key1 in  tree1.keys():
-			visited.add(key1)
-			if key1 in tree2.keys():
-				if isinstance(tree1[key1], dict) == True:
-					if isinstance(tree2[key1], dict) == True:
-						string.append({
-                	           	"m": {
-                	           	"first": {"parent": key1,
-                	           	"children": walk(tree1[key1], tree2[key1], "u")}}})
-					else:
-						string.append({
-                                	flag: {
-                                	"first": {"parent": key1, "children": walk(tree1[key1], {}, flag)}, 
-                                	"second": {"key": key1, "value": tree2[key1] }}})
-				else:
-					if isinstance(tree2[key1], dict) == True:
-						string.append({
-                                		flag: {
-                                		"first": {"key": key1, "value": tree1[key1]}, 
-                                		"second": {"parent": key1, "children": walk(tree2[key1], {}, flag) }}})
-					else:
-						if tree1[key1] == tree2[key1]:
-							string.append({
-                                    	"m": {
-                                    	"first": {"key": key1, "value": tree1[key1]}}})
-						else:
-							string.append({
-                                    	flag: {
-                                    	"first": {"key": key1, "value": tree1[key1]}, 
-                                    	"second": {"key": key1, "value": tree2[key1]}}})
-			else:
-				if isinstance(tree1[key1], dict) == True:
-					string.append({
-                            	flag: {
-                            	"first": {"parent": key1, "children": walk(tree1[key1], {}, flag)}}})
-				else:
-					string.append({
-                            	flag: {
-                            	"first": {"key": key1, "value": tree1[key1]}}})
-		return (flag, string, visited)
-		
-		
+    """This function is the part of WALK to avoid linter complex error """
+    for key1 in tree1.keys():
+        visited.add(key1)
+        if key1 in tree2.keys():
+            string, flag = if_key1_in_tree2(key1, tree1, tree2, flag, string)
+        else:
+            if isinstance(tree1[key1], dict):
+                string.append({
+                            flag: {
+                                   "first": {
+                                             "parent": key1,
+                                             "children":
+                                             walk(tree1[key1],
+                                                  {}, flag)}}})
+            else:
+                string.append({
+                            flag: {
+                                   "first": {
+                                             "key": key1,
+                                             "value": tree1[key1]}}})
+        return flag, string, visited
+
+
 def second_tree(tree2, visited, string):
-		for key2 in tree2.keys():
-			if key2 in visited:
-				pass
-			elif isinstance(tree2[key2], dict) == True:
-				string.append({
+    """This function is the part of WALK to avoid linter complex error """
+    for key2 in tree2.keys():
+        if key2 in visited:
+            pass
+        elif isinstance(tree2[key2], dict):
+            string.append({
+                        "u": {
+                             "second": {
+                                             "parent": key2,
+                                             "children":
+                                             walk(tree2[key2],
+                                                  {}, "u")}}})
+        else:
+            string.append({
                             "u": {
-                            "second": {"parent": key2, "children": walk(tree2[key2], {}, "u") }}})
-			else:
-				string.append({
-                            "u": {
-                            "second": {"key": key2, "value": tree2[key2]}}})
-		return string
+                                 "second": {
+                                                 "key": key2,
+                                                 "value": tree2[key2]}}})
+    return string
 
 
 def get_diff_dict(data1, data2):
-	return formatter1(walk(data1, data2, "u"))
+    """get files objects and transfer them for
+    comparsion to WALK,
+    then result of WALK transfer to FORMATTER
+    Ready string from FORMATTER return to
+    GENERATE_DICT"""
+    return formatter1(walk(data1, data2, "u"))
