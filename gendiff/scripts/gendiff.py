d897e1ad0b989a208308cb3c5882e885167e7f95
NoFate35 <vsokol35@mail.ru>
2024-06-04 18:06:23 +0300

    Changes to be committed: 	modified:   gendiff/scripts/gendiff.py 	fix remarks from M

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,7 @@ from gendiff.diff.get_diff import generate_diff
 def main():
     descript = 'Compares two configuration files and shows a difference.'
     parser = argparse.ArgumentParser(description=descript)
-    parser.add_argument('first_file', type=str)
+    parser.add_argument('first_file')
     parser.add_argument('second_file', type=str)
     parser.add_argument("-f", "--format", type=str, help="set format of output")
     args = parser.parse_args()
